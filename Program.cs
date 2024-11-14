0d5f80a7d095e144c19bb795b4684ffa031413ba
JaoRicardo <joaopedroricardosouza2005@gmail.com>
2024-11-14 16:00:15 -0400

    adicionando com versão da aplicação

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine(DateTime.Now.ToLongDateString());
+Console.WriteLine("Versão da Aplicação 1.0");
